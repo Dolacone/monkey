549d508a942ec0bd4f21b3b563bcfdbb00322e3c
Dolacone <dolacone@gmail.com>
2024-07-02 12:30:00 +0800

    hide non-targets

--- crime-pickpocket.js
+++ crime-pickpocket.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Torn: Pickpocket Targets
-// @version      0.2.2
+// @version      0.2.3
 // @description  Highlight Pickpocket targets
 // @author       Dola [2720731]
 // @match        https://www.torn.com/loader.php?sid=crimes*
@@ -26,14 +26,16 @@
 
         const rows = document.querySelectorAll('.crime-option:not(.processed)')
         rows.forEach(row => {
-          const name = row.querySelector('div .titleAndProps___DdeVu > div:first-child').textContent.trim();
-          if (markGroups.some(target => name.includes(target))) {
-              row.style.borderLeft = `3px solid #37b24d`;
-              row.style.background = 'darkgreen';
-              row.querySelector('div .childrenWrapper___h2Sw5').style.color = '#37b24d';
-              sound.play();
-          };
-          row.classList.add('processed');
+            const name = row.querySelector('div .titleAndProps___DdeVu > div:first-child').textContent.trim();
+            if (markGroups.some(target => name.includes(target))) {
+                row.style.borderLeft = `3px solid #37b24d`;
+                row.style.background = 'darkgreen';
+                row.querySelector('div .childrenWrapper___h2Sw5').style.color = '#37b24d';
+                sound.play();
+            } else {
+                row.style.display = 'none';
+            };
+            row.classList.add('processed');
         });
     };
 
