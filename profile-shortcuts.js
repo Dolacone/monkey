16acf9cbc6274b8911a3e485b3b878a6fe43f56e
Dolacone <dolacone@gmail.com>
2025-03-10 14:54:03 +0800

    add alert for ok targets

--- profile-shortcuts.js
+++ profile-shortcuts.js
@@ -1,13 +1,17 @@
 // ==UserScript==
 // @name         Torn: Profile shortcuts
-// @version      0.1.0
+// @version      0.2.0
 // @description
 // @author       Dola [2720731]
 // @match        https://www.torn.com/profiles.php?*
 // @downloadURL  https://raw.githubusercontent.com/Dolacone/torn/master/profile-shortcuts.js
+// @updateURL    https://raw.githubusercontent.com/Dolacone/torn/master/profile-shortcuts.js
 // @icon
 // @grant        none
 // ==/UserScript==
+
+let sound = new Audio('https://cdn.pixabay.com/download/audio/2024/05/23/audio_336d55dfa8.mp3?filename=servant-bell-ring-2-211683.mp3');
+let monitorStatusInterval = null;
 
 function keypressHandler(event) {
     event.preventDefault();
@@ -18,7 +22,28 @@ function keypressHandler(event) {
     }
 }
 
+function monitorStatus() {
+    const status = document.querySelector('.description .main-desc').innerText
+    const status_sub = document.querySelector('.description .sub-desc').innerText
+    if (status === "Okay"){
+        document.body.style.backgroundColor = 'red';
+        document.title = "attack";
+        sound.play();
+        clearInterval(monitorStatusInterval);
+    }
+    if (status.startsWith('In ') && status_sub === ""){
+        document.body.style.backgroundColor = 'red';
+        document.title = "attack";
+        sound.play();
+        clearInterval(monitorStatusInterval);
+    }
+}
+
 (function() {
     'use strict';
     document.addEventListener('keypress', keypressHandler);
+
+    monitorStatusInterval = setInterval(() => {
+        monitorStatus();
+    }, 1000);
 })();
